8af38b2d524620d1845a8d763dd7b369ee0dbeec
p-rodriguezm <90836437+p-rodriguezm@users.noreply.github.com>
2021-11-26 16:26:07 +0100

    Add files via upload

--- claseMáquina/maquina.cc
+++ claseMáquina/maquina.cc
@@ -1,10 +1,17 @@
-/*
- * maquina.cc
- *
- *  Created on: 24 nov. 2021
- *      Author: asimancas
- */
+//maquina.cc
+
+#include "maquina.h"
+
+using namespace std;
 
 
+Maquina::Maquina(string id, int nucleos){
+	id_maq=id;
+	estado=true;
+	nuc_tot=nucleos;
+	nuc_disp=nucleos;
+}
 
-
+void setNucleosDisp(const int nucleos){
+	nuc_disp = nucleos;
+}
