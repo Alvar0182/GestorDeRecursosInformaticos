a388ddecbbb2b0aed35e0e00ac9e6a5a2af24e63
p-rodriguezm <90836437+p-rodriguezm@users.noreply.github.com>
2021-11-29 18:46:42 +0100

    Add files via upload

--- claseReserva/reserva.h
+++ claseReserva/reserva.h
@@ -1,15 +1,51 @@
-/*
- * reserva.h
- *
- *  Created on: 24 nov. 2021
- *      Author: asimancas
- */
+//reserva.h
 
-#ifndef CLASERESERVA_RESERVA_H_
-#define CLASERESERVA_RESERVA_H_
+#ifndef RESERVA_H
+#define RESERVA_H
+
+struct date{
+   int day;  
+   int month; 
+   int year;  
+}
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+class Reserva{
+	private:
+		string id_reser;
+		string creador_reser;
+		struct date duracion;  //duracion en dd/mm/aaaa
+		struct date fechaInicio;
+		struct date fechaFin;
+		int cant_nuc;
+		string maq_reser;
+
+	public:
+
+		Reserva(string id, string creador, struct date fIni, struct date fFin, int nucl, string maq);
+
+		inline string getId(){return id_reser;}
+
+		inline string getCreador(){return creador_reser;}
+
+		inline struct date getDuracion(){return duracion;}
+
+		inline struct date getFechaInicio(){return fechaInicio;}
+
+		inline struct date getFechaFin(){return fechaFin;}
+
+		inline int getCantNuc(){return cant_nuc;}
+
+		inline string getMaquina(){return maq_reser;}
+
+		void setFechas();
 
 
+};
 
-
-
-#endif /* CLASERESERVA_RESERVA_H_ */
+#endif
