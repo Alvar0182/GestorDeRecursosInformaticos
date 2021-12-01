49f0d49f3bfb909c1df1fdc7447b28fb3e51d080
Álvaro <90786828+Alvar0182@users.noreply.github.com>
2021-12-01 20:18:36 +0100

    Add files via upload

--- clase Administrador/administrador.h
+++ clase Administrador/administrador.h
@@ -4,11 +4,20 @@
  *  Created on: 24 nov. 2021
  *      Author: asimancas
  */
-
 #ifndef CLASE_ADMINISTRADOR_ADMINISTRADOR_H_
 #define CLASE_ADMINISTRADOR_ADMINISTRADOR_H_
 
+#include <string>
 
+class Administrador{
+private:
+	std:: string id_admin;
+
+public:
+	void verInfor();
+	void modificarReserva();
+	void borrarReserva();
+};
 
 
 
