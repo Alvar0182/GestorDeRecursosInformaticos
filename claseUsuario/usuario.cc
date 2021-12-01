7f416cfb3cb9892d37db328677a160fc26ec219a
Álvaro <90786828+Alvar0182@users.noreply.github.com>
2021-12-01 20:19:32 +0100

    Add files via upload

--- claseUsuario/usuario.cc
+++ claseUsuario/usuario.cc
@@ -5,6 +5,27 @@
  *      Author: asimancas
  */
 
+#include "usuario.h"
+#include "sistema.h"
+#include <iostream>
+#include "sistema.h"
+#include <list>
 
+void::Usuario crearReserva(std::string correoElectronico, int numNucleos, time_t tiempo){
 
+}
 
+void::Usuario modificarReserva(std::string id_reser,std::string correoElectronico, int numNucleos, time_t tiempo){
+
+}
+
+void::Usuario cancelarReserva(std::string id_reser){
+
+}
+
+void::Usuario visualizarReserva(){
+	//iterador it;
+	for(it=L.begin();it!=L.end();++it){
+
+	}
+}
--- claseUsuario/usuario.h
+++ claseUsuario/usuario.h
@@ -8,8 +8,22 @@
 #ifndef CLASEUSUARIO_USUARIO_H_
 #define CLASEUSUARIO_USUARIO_H_
 
+#include <string>
+#include <ctime>
+using namespace std;
 
-
-
-
+class Usuario{
+private:
+	string idUsu;
+	string correo;
+	time_t lim_tiempo;
+	int lim_nucleo;
+	time_t tiempoDisp;
+	int nucleoDisp;
+public:
+	void crearReserva(std::string correoElectronico, int numNucleos, time_t tiempo);
+	void modificarReserva(std::string id_reser,std::string correoElectronico, int numNucleos, time_t tiempo);
+	void cancelarReserva(std::string id_reser);
+	void visualizarReserva();
+};
 #endif /* CLASEUSUARIO_USUARIO_H_ */
