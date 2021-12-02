14e54efd6ffbbc6b087a74d21491f69ad9fbb2ef
Álvaro <90786828+Alvar0182@users.noreply.github.com>
2021-12-02 17:28:32 +0100

    Clase Usuario hecha. Faltan los tests, se hacen en el siguiente sprint

--- claseUsuario/usuario.cc
+++ claseUsuario/usuario.cc
@@ -1,31 +1,59 @@
-/*
- * usuario.cc
- *
- *  Created on: 24 nov. 2021
- *      Author: asimancas
- */
-
 #include "usuario.h"
-#include "sistema.h"
+#include "reserva.h"
 #include <iostream>
-#include "sistema.h"
 #include <list>
 
 void::Usuario crearReserva(std::string correoElectronico, int numNucleos, time_t tiempo){
-
+	Reserva r;
+	r.setCorreoElectronico(correoElectronico);
+	r.setnumNucleos(numNucleos);
+	r.setTiempo(tiempo);
+	reservas_.push_back(r);
 }
 
 void::Usuario modificarReserva(std::string id_reser,std::string correoElectronico, int numNucleos, time_t tiempo){
+	std::list<Reserva>::iterator r;
+	r=reservas_.begin();
+	for(int i=0;reservas_.size()>i;i++){
+		if(id_reser=(*r).getIdReser()){
+			std::cout<<"Introduzca los nuevos datos de la reserva"<<std::endl;
+			std::string correo;
+			int nucleos;
+			time_t time;
+			std::cout<<"Introduzca su correo electronico: ";
+			std::cin>>correo;
+			std::cout<<std::endl;
+			std::cout<<"Introduzca el numero de nucleos que desea reservar: ";
+			std::cin>>nucleos;
+			std::cout<<std::endl;
+			std::cout<<"Introduzca el tiempo que desea reservar: ";
+			std::cin>>time;
+			std::cout<<std::endl;
 
+			Reserva res;
+			res.setIdReser(id_reser);
+			res.setCorreoElectronico(correo);
+			res.setnumNucleos(nucleos);
+			res.setTiempo(time);
+			reservas_.push_back(res)
+		}
+		j++;
+	}
 }
 
 void::Usuario cancelarReserva(std::string id_reser){
-
-}
-
-void::Usuario visualizarReserva(){
-	//iterador it;
-	for(it=L.begin();it!=L.end();++it){
-
+	std::list<Reserva>::iterator r;
+	r=reservas_begin();
+	for(int i=0;reservas_.size()>i;i++){
+		if(id_reser=(*r).getIdReser()){
+			std::string correo="";
+			int nucleos=0;
+			time_t tiempo=0;
+			Reserva res;
+			res.setCorreoElectronico(correo);
+			res.setnumNucleos(nucleos);
+			res.setTiempo(tiempo);
+		}
+		r++;
 	}
 }
--- claseUsuario/usuario.h
+++ claseUsuario/usuario.h
@@ -1,10 +1,3 @@
-/*
- * usuario.h
- *
- *  Created on: 24 nov. 2021
- *      Author: asimancas
- */
-
 #ifndef CLASEUSUARIO_USUARIO_H_
 #define CLASEUSUARIO_USUARIO_H_
 
@@ -24,6 +17,5 @@ public:
 	void crearReserva(std::string correoElectronico, int numNucleos, time_t tiempo);
 	void modificarReserva(std::string id_reser,std::string correoElectronico, int numNucleos, time_t tiempo);
 	void cancelarReserva(std::string id_reser);
-	void visualizarReserva();
 };
 #endif /* CLASEUSUARIO_USUARIO_H_ */
