abf46307ea4405ef9cb0782942668404f888400d
Álvaro <90786828+Alvar0182@users.noreply.github.com>
2021-12-03 17:37:29 +0100

    Añadidos setters y getters

--- claseUsuario/usuario.h
+++ claseUsuario/usuario.h
@@ -17,5 +17,16 @@ public:
 	void crearReserva(std::string correoElectronico, int numNucleos, time_t tiempo);
 	void modificarReserva(std::string id_reser,std::string correoElectronico, int numNucleos, time_t tiempo);
 	void cancelarReserva(std::string id_reser);
-};
+	inline string getIdUsu(){return idUsu;}
+	inline void setIdUsu(string id){idUsu=id;}
+	inline string getCorreo(){return correo;}
+	inline void setCorreo(string cor){correo=cor;}
+	inline time_t getLim_tiempo(){return lim_tiempo;}
+	inline void setLim_tiempo(time_t tiempo){lim_tiempo=tiempo;}
+	inline int getLim_nucleo(){return lim_nucleo;}
+	inline void setLim_nucleo(int limite){lim_nucleo=limite;}
+	inline time_t getTiempoDisp(){return tiempoDisp;}
+	inline void setTiempoDisp(time_t tiempo){tiempoDisp=tiempo;}
+	inline int getNucleoDisp(){return nucleoDisp;}
+	inline void setNucleoDisp(int disp){nucleoDisp=disp;}
 #endif /* CLASEUSUARIO_USUARIO_H_ */
