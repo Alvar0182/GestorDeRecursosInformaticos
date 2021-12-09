55844f5f9209286c606bffbb581264e6a68197db
p-rodriguezm <90836437+p-rodriguezm@users.noreply.github.com>
2021-12-09 09:20:19 +0100

    Modificada funcion BajaUsuarios
    Ahora elimina las reservas del usuario a borrar

--- BajaUsuario/bajaUsuario.cc
+++ BajaUsuario/bajaUsuario.cc
@@ -1,15 +1,25 @@
 void bajaUsuario(){
 	//pedir usuario
-  
-	  string id;
-	  cin>>id;
+	string id;
+	cin>>id;
 	//buscar usuario
-  
-	  list<Usuario>::iterator i;
+	int existe=0;
+	list<Usuario>::iterator i;
 		for(i=usuarios.begin();i!=usuarios.end();i++){
-			  if(id==i->getIdUsu()){
-	//eliminar usuario
+			if(id==i->getIdUsu()){
+
+				//eliminar reservas que tenga el usuario
+					list<Reserva>::iterator it;
+					for(it=reservas.begin();it!=reservas.end();it++){
+						if(i->getIdUsu()==it->getCreador()){
+							reservas.erase(it);
+						}
+					}
+
+				//eliminar usuario
 
 				usuarios.erase(i);		
-		    }
-    }
+
+		}
+
+}
