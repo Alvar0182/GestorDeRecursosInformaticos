27688ce954f960de713e0d7d50be94ec33d7930b
p-rodriguezm <90836437+p-rodriguezm@users.noreply.github.com>
2021-12-11 16:01:45 +0100

    modificado reserva.h
    cambiado los id a string

--- claseReserva/reserva.h
+++ claseReserva/reserva.h
@@ -2,24 +2,25 @@
 #define RESERVA_H
 #include <ctime>
 #include <list>
+#include <string>
 
 class Reserva{
     private:
-    int id_reser;
-    int creador_reserva;
+    string id_reser;
+    string creador_reserva;
     time_t tiempo;
     int nuc_reser;
-    int maq_reser;
+    string maq_reser;
 
     public:
 
-    Reserva(int id,int creador,time_t tiempo_,int nuc,int maq);
+    Reserva(string id,string creador,time_t tiempo_,int nuc,string maq);
 
-    int getId_reser(){return id_reser;}
-    void setId_reser(int id){id_reser=id;}
+    string getId_reser(){return id_reser;}
+    void setId_reser(string id){id_reser=id;}
 
-    int getCreador_reserva(){return creador_reserva;}
-    void setCreador_reserva(int creador){creador_reserva=creador;}
+    string getCreador_reserva(){return creador_reserva;}
+    void setCreador_reserva(string creador){creador_reserva=creador;}
 
     time_t getTiempo(){return tiempo;}
     void setTiempo(time_t time){tiempo=time;}
@@ -27,7 +28,7 @@ class Reserva{
     int getNuc_reser(){return nuc_reser;}
     void setNuc_reser(int nuc){nuc_reser=nuc;}
     
-    int getMaq_reser(){return maq_reser;}
-    void setMaq_reser(int maq){maq_reser=maq;}
+    string getMaq_reser(){return maq_reser;}
+    void setMaq_reser(string maq){maq_reser=maq;}
 };
 #endif
