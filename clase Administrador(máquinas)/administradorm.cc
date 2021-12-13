6014dadb90d57d667fa1f0dc46ff3f86ae4dd76b
p-rodriguezm <90836437+p-rodriguezm@users.noreply.github.com>
2021-12-13 13:00:38 +0100

    Añadida funcion en clase correspondiente

--- clase Administrador(máquinas)/administradorm.cc
+++ clase Administrador(máquinas)/administradorm.cc
@@ -5,6 +5,36 @@
  *      Author: asimancas
  */
 
+#include "administradorm.h"
 
+
+void bajaMaq(){
+
+	//pedir maquina
+
+		string id;
+		cin>>id;
+
+	//buscar maquina
+
+		list<Maquina>::iterator i;
+			for(i=maquinas.begin();i!=maquinas.end();i++){
+				if(id==i->getIdMaq()){
+
+				//eliminar reservas asignada a la maquina
+					list<Reserva>::iterator it;
+					for(it=reservas.begin();it!=reservas.end();it++){
+						if(i->getIdMaq()==it->getMaq_reser()){
+							reservas.erase(it);
+						}
+					}
+
+	//eliminar maquina
+
+				maquinas.erase(i);		
+		}
+
+			}
+}
 
 
