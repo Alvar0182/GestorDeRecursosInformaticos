8c0d1b04a8cb408ba06784d23bb552a11cec697e
angelnr <90841264+angelnr@users.noreply.github.com>
2021-12-16 19:27:27 +0100

    Cambios en la nomenclatura
    Los parámetros privados acaban en una _ para dar unicidad al código

--- claseReserva/reserva.h
+++ claseReserva/reserva.h
@@ -1,35 +1,34 @@
 #ifndef RESERVA_H
 #define RESERVA_H
-#include <ctime>
 #include <list>
 #include <string>
 using namespace std;
 
 class Reserva{
     private:
-    string id_reser;
-    string creador_reserva;
-    time_t tiempo;
-    int nuc_reser;
-    string maq_reser;
+    string id_reser_;
+    string creador_reserva_;
+    int tiempo_;
+    int nuc_reser_;
+    string maq_reser_;
 
     public:
 
-    Reserva(string id,string creador,time_t tiempo_,int nuc,string maq);
+    Reserva(string id,string creador,int tiempo,int nuc,string maq);
 
-    string getId_reser(){return id_reser;}
-    void setId_reser(string id){id_reser=id;}
+    string getId_reser(){return id_reser_;}
+    void setId_reser(string id){id_reser_=id;}
 
-    string getCreador_reserva(){return creador_reserva;}
-    void setCreador_reserva(string creador){creador_reserva=creador;}
+    string getCreador_reserva(){return creador_reserva_;}
+    void setCreador_reserva(string creador){creador_reserva_=creador;}
 
-    time_t getTiempo(){return tiempo;}
-    void setTiempo(time_t time){tiempo=time;}
+    int getTiempo(){return tiempo_;}
+    void setTiempo(int time){tiempo_=time;}
 
-    int getNuc_reser(){return nuc_reser;}
-    void setNuc_reser(int nuc){nuc_reser=nuc;}
+    int getNuc_reser(){return nuc_reser_;}
+    void setNuc_reser(int nuc){nuc_reser_=nuc;}
     
-    string getMaq_reser(){return maq_reser;}
-    void setMaq_reser(string maq){maq_reser=maq;}
+    string getMaq_reser(){return maq_reser_;}
+    void setMaq_reser(string maq){maq_reser_=maq;}
 };
 #endif
