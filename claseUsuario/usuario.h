9a3a0a4ade814cfd4a92ee54116039e0852d2140
p-rodriguezm <90836437+p-rodriguezm@users.noreply.github.com>
2021-12-16 19:50:51 +0100

    Cambio formato de tiempo en clase usuario

--- claseUsuario/usuario.h
+++ claseUsuario/usuario.h
@@ -1,33 +1,35 @@
 #ifndef CLASEUSUARIO_USUARIO_H_
 #define CLASEUSUARIO_USUARIO_H_
 
+#include <iostream>
+#include <cstdlib>
 #include <string>
-#include <ctime>
+
 using namespace std;
 
 class Usuario{
 private:
 	string idUsu;
 	string correo;
-	time_t lim_tiempo;
+	int lim_tiempo;
 	int lim_nucleo;
-	time_t tiempoDisp;
+	int tiempoDisp;
 	int nucleoDisp;
 public:
 	Usuario(string idUsuario, string correo, int limTiempo, int limNucleo, int tDisp, int nDisp);
-	void crearReserva(std::string correoElectronico, int numNucleos, time_t tiempo);
+	/*void crearReserva(std::string correoElectronico, int numNucleos, time_t tiempo);
 	void modificarReserva(std::string id_reser,std::string correoElectronico, int numNucleos, time_t tiempo);
-	void cancelarReserva(std::string id_reser);
+	void cancelarReserva(std::string id_reser);*/
 	inline string getIdUsu(){return idUsu;}
 	inline void setIdUsu(string id){idUsu=id;}
 	inline string getCorreo(){return correo;}
 	inline void setCorreo(string cor){correo=cor;}
-	inline time_t getLim_tiempo(){return lim_tiempo;}
-	inline void setLim_tiempo(time_t tiempo){lim_tiempo=tiempo;}
+	inline ing getLim_tiempo(){return lim_tiempo;}
+	inline void setLim_tiempo(int tiempo){lim_tiempo=tiempo;}
 	inline int getLim_nucleo(){return lim_nucleo;}
 	inline void setLim_nucleo(int limite){lim_nucleo=limite;}
-	inline time_t getTiempoDisp(){return tiempoDisp;}
-	inline void setTiempoDisp(time_t tiempo){tiempoDisp=tiempo;}
+	inline int getTiempoDisp(){return tiempoDisp;}
+	inline void setTiempoDisp(int tiempo){tiempoDisp=tiempo;}
 	inline int getNucleoDisp(){return nucleoDisp;}
 	inline void setNucleoDisp(int disp){nucleoDisp=disp;}
 #endif /* CLASEUSUARIO_USUARIO_H_ */
