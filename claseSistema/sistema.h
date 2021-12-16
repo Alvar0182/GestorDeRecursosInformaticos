78004f8e4cb6df49e5c9337f9a8c97fbaec7a62d
Carlos0022 <90833008+Carlos0022@users.noreply.github.com>
2021-12-16 22:59:45 +0100

    Update sistema.h

--- claseSistema/sistema.h
+++ claseSistema/sistema.h
@@ -1,13 +1,31 @@
-/*
- * sistema.h
- *
- *  Created on: 24 nov. 2021
- *      Author: asimancas
- */
-
 #ifndef CLASESISTEMA_SISTEMA_H_
 #define CLASESISTEMA_SISTEMA_H_
 
+#include <iostream>
+#include <cstdlib>
+
+
+class Sistema{
+	
+	private:
+		
+        bool existeReserva;
+        bool existeUsuario;
+        bool existeMaquina;
+        bool pasaLimite;
+        bool sufcNuc;
+	public:
+		
+		Sistema(bool existeReserva, bool existeUsuario, bool existeMaquina, bool pasaLimite, bool sufcNuc);
+
+		inline bool comprobarReserva(const bool existeReserva){if(existeReserva=true){return true;}else{return false;})} 
+		inline bool comprobarUsuario(const bool existeUsuario){if(existeUsuario=true){return true;}else{return false;})} 
+		inline bool comprobarMaquina(const bool existeMaquina){if(existeMaquina=true){return true;}else{return false;})}
+        inline bool comprobarLimite(const bool pasaLimite){if(pasaLimite=true){return true;}else{return false;})}
+        inline bool quedanNuc(const bool sufcNuc){if(sufcNuc=true{return true;}else{return false;})};
+
+		
+};
 
 
 
