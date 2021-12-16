00beab851c3970232761f8783b2c579b8beb50bf
angelnr <90841264+angelnr@users.noreply.github.com>
2021-12-16 19:29:04 +0100

    Cambio en la nomenclatura de las variables

--- claseReserva/reserva.cc
+++ claseReserva/reserva.cc
@@ -1,9 +1,9 @@
 #include "reserva.h"
 
-Reserva::Reserva(string id, string creador,time_t tiempo_,int nuc, string maq){
-    id_reser=id;
-    creador_reserva=creador;
-    tiempo=tiempo_;
-    nuc_reser=nuc;
-    maq_reser=maq;
+Reserva::Reserva(string id, string creador,int  tiempo,int nuc, string maq){
+    id_reser_=id;
+    creador_reserva_=creador;
+    tiempo_=tiempo_;
+    nuc_reser_=nuc;
+    maq_reser_=maq;
 }
