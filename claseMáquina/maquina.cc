3afd85705c91a63231b99fca6e38fb54a5603110
p-rodriguezm <90836437+p-rodriguezm@users.noreply.github.com>
2021-12-16 13:55:43 +0100

    Edicion de maquina.cc para evitar errores
    Con este cambio evitamos posibles datos erroneos

--- claseMáquina/maquina.cc
+++ claseMáquina/maquina.cc
@@ -4,10 +4,15 @@
 
 using namespace std;
 
-
 Maquina::Maquina(string id, int nucleos){
 	id_maq=id;
-	estado=true;
-	nuc_tot=nucleos;
-	nuc_disp=nucleos;
+	if(nucleos>0){
+		estado=true;
+		nuc_tot=nucleos;
+		nuc_disp=nucleos;}
+	else{
+		estado=false;
+		nuc_tot=0;
+		nuc_disp=0;
+	}
 }
